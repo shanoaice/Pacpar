e06e5139a8f8812782c35a22bc06f07f3bcaaa8a
shanoaice <shanoaice@noreply.codeberg.org>
2025-10-21 00:23:24 -0700

    feature(alpm): AlpmList Enumerator

--- Pacpar.Alpm/AlpmList.cs
+++ Pacpar.Alpm/AlpmList.cs
@@ -1,7 +1,61 @@
-namespace Pacpar.Alpm
+using System.Collections;
+
+namespace Pacpar.Alpm;
+
+/// <summary>
+/// Wraps an alpm_list_t from libalpm.
+/// **Warning**: You will need to provide a factory method that 
+/// constructs the type T from the native pointer.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class AlpmList<T> : IDisposable, ICollection<T> where T : IDisposable
 {
-  public class AlpmList
+  private unsafe delegate T _factoryType(void* ptr);
+
+  private unsafe readonly _alpm_list_t* _alpmListNative;
+  private readonly _factoryType _factory;
+
+  public class AlpmListEnumerator<TEnum>(AlpmList<TEnum> _alpmList) : IEnumerator<TEnum> where TEnum : IDisposable
   {
-    public string[] Items { get; set; }
+    private unsafe _alpm_list_t* _alpmListNative = _alpmList._alpmListNative;
+
+    public unsafe TEnum Current
+    {
+      get
+      {
+        return _alpmList._factory(_alpmListNative->data);
+      }
+    }
+
+    public unsafe bool MoveNext()
+    {
+      var next = NativeMethods.alpm_list_next(_alpmListNative);
+      if ((IntPtr)next == IntPtr.Zero) return false;
+      _alpmListNative = next;
+      return true;
+    }
+
+    public unsafe void Reset()
+    {
+      _alpmListNative = _alpmList._alpmListNative;
+    }
+
+    public void Dispose()
+    {
+      GC.SuppressFinalize(this);
+    }
+
+    object IEnumerator.Current => Current;
   }
+
+  public void Dispose()
+  {
+
+  }
+
+  public IEnumerator<T> GetEnumerator() => new AlpmListEnumerator<T>(this);
+
+  IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+  public string[] Items { get; set; }
 }
