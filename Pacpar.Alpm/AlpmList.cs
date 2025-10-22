022454ffac2df8fea88c7717c649ed20f0d829ad
shanoaice <shanoaice@noreply.codeberg.org>
2025-10-22 11:11:34 -0700

    feature(alpm-list): owned list head constructor

--- Pacpar.Alpm/AlpmList.cs
+++ Pacpar.Alpm/AlpmList.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace Pacpar.Alpm;
 
@@ -6,42 +7,65 @@ namespace Pacpar.Alpm;
 /// Wraps an alpm_list_t from libalpm.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public class AlpmList<T> : IDisposable, ICollection<T> where T : IDisposable
+public class AlpmList<T> : IDisposable, IEnumerable<T> where T : IDisposable
 {
-  public unsafe delegate T _factoryType(void* ptr);
+  internal unsafe _alpm_list_t* _alpmListNative;
+  private unsafe readonly delegate*<void*, T> _factory;
 
-  private unsafe readonly _alpm_list_t* _alpmListNative;
-  private readonly _factoryType _factory;
+  private readonly bool _ownsPtr = false;
+  private bool _disposed = false;
 
-  internal unsafe AlpmList(_alpm_list_t* alpmList, _factoryType factory)
+  /// <summary>
+  /// Wraps an existing alpm_list_t* owned by libalpm (accross FFI boundaries).
+  /// *Note*: This is intended for internal wrapping only. 
+  /// </summary>
+  /// <param name="alpmList">existing alpm_list_t*</param>
+  /// <param name="factory">factory function to covert void* to T</param>
+
+  internal unsafe AlpmList(_alpm_list_t* alpmList, delegate*<void*, T> factory)
   {
-    this._alpmListNative = alpmList;
-    this._factory = factory;
+    _alpmListNative = alpmList;
+    _factory = factory;
+  }
+
+  /// <summary>
+  /// Creates and wraps a new alpm_list_t* list head, owned by dotnet. 
+  /// Note that subsequent list is probably owned across FFI boundraies 
+  /// when sent to libalpm for item dumping, thus we only own the head.
+  /// The data is also probably owned by libalpm.
+  /// </summary>
+  /// <param name="factory">factory function to covert void* to T</param>
+  public unsafe AlpmList(delegate*<void*, T> factory)
+  {
+    _factory = factory;
+    var alpmListSize = Marshal.SizeOf<_alpm_list_t>();
+    _alpmListNative = (_alpm_list_t*)Marshal.AllocHGlobal(alpmListSize);
+    _ownsPtr = true;
   }
 
   public class AlpmListEnumerator<TEnum>(AlpmList<TEnum> _alpmList) : IEnumerator<TEnum> where TEnum : IDisposable
   {
-    private unsafe _alpm_list_t* _alpmListNative = _alpmList._alpmListNative;
+    private unsafe _alpm_list_t* _alpmList_native = _alpmList._alpmListNative;
 
     public unsafe TEnum Current
     {
       get
       {
-        return _alpmList._factory(_alpmListNative->data);
+        return _alpmList._factory(_alpmList_native->data);
       }
     }
 
     public unsafe bool MoveNext()
     {
-      var next = NativeMethods.alpm_list_next(_alpmListNative);
+      var next = NativeMethods.alpm_list_next(_alpmList_native);
       if ((IntPtr)next == IntPtr.Zero) return false;
-      _alpmListNative = next;
+      _alpmList_native = next;
       return true;
     }
 
     public unsafe void Reset()
     {
-      _alpmListNative = _alpmList._alpmListNative;
+      _alpmList_native = _alpmList._alpmListNative;
     }
 
     public void Dispose()
