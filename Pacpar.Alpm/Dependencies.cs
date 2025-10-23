782d0c02b8deb92b63cc2158d17a2f9268a23b89
shanoaice <shanoaice@noreply.codeberg.org>
2025-10-23 10:23:45 -0700

    feature(alpm_deps): wrapper lazy-property

---
+++ Pacpar.Alpm/Dependencies.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Pacpar.Alpm;
+
+public class AlpmDepend
+{
+  internal unsafe _alpm_depend_t* _backing_struct;
+
+  private string? _description;
+  private string? _name;
+  private string? _version;
+
+  public unsafe string? Description
+  {
+    get
+    {
+      _description ??= Marshal.PtrToStringUTF8((IntPtr)_backing_struct->desc);
+      return _description;
+    }
+  }
+  public unsafe string? Name
+  {
+    get
+    {
+      _name ??= Marshal.PtrToStringUTF8((IntPtr)_backing_struct->name);
+      return _name;
+    }
+  }
+  public unsafe string? Version
+  {
+    get
+    {
+      _version ??= Marshal.PtrToStringUTF8((IntPtr)_backing_struct->version);
+      return _version;
+    }
+  }
+
+  public unsafe _alpm_depmod_t Depmod => _backing_struct->mod_;
+}
