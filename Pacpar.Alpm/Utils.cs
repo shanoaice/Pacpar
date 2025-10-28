821ff6d3331087027a7e97bf95a2298dce0fa36a
shanoaice <shanoaice@noreply.codeberg.org>
2025-10-28 00:20:37 -0700

    feat(utils): hopefully correct c free binding

---
+++ Pacpar.Alpm/Utils.cs
@@ -0,0 +1,11 @@
+#pragma warning disable SYSLIB1054
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Pacpar.Alpm;
+
+public static class Utils
+{
+  [DllImport("libc", EntryPoint = "free", CallingConvention = CallingConvention.Cdecl)]
+  internal extern unsafe static void CFree(void* ptr);
+}
